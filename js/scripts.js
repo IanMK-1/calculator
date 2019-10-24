5d52efc104b91b59cd1452674a3c3ea1ff3c6e54
ian mwangi <iankimotho233@gmail.com>
2019-10-24 09:49:19 +0300

    Add the html boilerplate and the addition js file

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var add = function(number1,number2){
+return number1+number2;
+};
+var number1=parseInt(prompt("Enter the first number:"));
+var number2=parseInt(prompt("Enter the second number:"));
+alert("The total is: "+add(number1,number2));
